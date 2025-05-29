e3e9f9065be2df28cadf8fb8e4fd3ba241b9833e
Alicia-Natasha <alicia.natasha@student.moringaschool.com>
2025-05-30 01:37:46 +0300

    operations

--- lib/models/operations.py
+++ lib/models/operations.py
@@ -1,4 +1,17 @@
+from models import User
+from models import Books
+from models import Loan
+from sqlalchemy.orm import Session
+from db import engine
+import datetime
+
 class Operations:
 
     def add_user(name):
-        pass
+        """Add a new user to the database."""
+        with Session(engine) as session:
+            new_user = User(name=name)
+            session.add(new_user)
+            session.commit()
+            print(f"User {name} added with ID {new_user.id}")
+            
