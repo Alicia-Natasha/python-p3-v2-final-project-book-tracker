e6d74fb4d17c2b4ddce373ed3745e84596e560ba
Alicia-Natasha <alicia.natasha@student.moringaschool.com>
2025-05-30 01:43:07 +0300

    operations

--- lib/models/operations.py
+++ lib/models/operations.py
@@ -23,4 +23,23 @@ class Operations:
             session.commit()
             print(f"Book '{title}' by {author} added with ID {new_book.id}")
 
+    def check_out_book(user_id, book_title):
+        """Check out a book for a user."""
+        with Session(engine) as session:
+            book = session.query(Books).filter_by(Books.id == book_title, is_checked_out=False).first()
+            if not book or book.checked_out:
+                print(f"Book {book_title} is not available for checkout.")
+                return
+            
+            user = session.query(User).filter(User.id == user_id).first()
+            if not user:
+                print(f"User {user_id} does not exist.")
+                return
+            
+            new_loan = Loan(user_id=user.id, book_title=book.title)
+            book.checked_out = True
+            session.add(new_loan)
+            session.commit()
+            print(f"Book '{book.title}' checked out to User ID {user.id}.")
+
     
